7382a42c991efa1c940a0f6a1fb8c2bc615be3db
WaGaNaWa <m4a1carbin4@gmail.com>
2023-12-17 21:12:05 +0900

    update

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-python main.py --kafka --kafka_producer_topic test_llm --kafka_consumer_topic test_llm_input --quantized_model_dir WGNW/llama-2-7b-ko-auto-gptq-full-v2 --early_stopping --max_new_token 256 --human_str "### 시청자" --ai_str "### 전파녀" --stop_str "### 시청자:"
+pyenv virtualenv 3.11 LightKorLLM
 
-#python main.py --base_dir ./ --model_name G_67000.pth --config_name Arona.json
+pyenv activate LightKorLLM
+
+pip install -r requirements.txt
+
+python main.py --kafka --kafka_producer_topic test_llm --kafka_consumer_topic test_llm_input --quantized_model_dir WGNW/llama-2-7b-ko-auto-gptq-full-v2 --early_stopping --max_new_token 256 --human_str $1 --ai_str $2 --stop_str $3
