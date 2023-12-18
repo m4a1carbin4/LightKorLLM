d0d55aee60ff32e5a5380ee095a32129aa5a9314
WaGaNaWa <m4a1carbin4@gmail.com>
2023-12-18 23:55:53 +0900

    update readme.md

--- start.sh
+++ start.sh
@@ -23,8 +23,8 @@ docker rmi -f ${db_image_name}:${version}
 
 # new-build/re-build docker image
 echo "=> Build new image..."
-docker build --build-arg kafka_producer_topic="$3" --build-arg kafka_consumer_topic="$4" --build-arg kafka_server="$5" --build-arg ai_str="$2" --build-arg human_str="$1" --tag ${db_image_name}:${version} .
+docker build --no-cache --build-arg kafka_producer_topic="$3" --build-arg kafka_consumer_topic="$4" --build-arg kafka_server="$5" --build-arg ai_str="$2" --build-arg human_str="$1" --tag ${db_image_name}:${version} .
 
 # Run container
 echo "=> Run container..."
-docker run -it --net=host --name ${db_container_name} --gpus all ${db_image_name}:${version}
+docker run -it --net=host --name ${db_container_name} ${db_image_name}:${version}
